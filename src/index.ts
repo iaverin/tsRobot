d0cca082d9c88a0b2e4770e6de66207702f6a91e
Ivan Averin <i.averin@gmail.com>
2021-08-28 15:38:23 +0300

    added more examples

--- src/index.ts
+++ src/index.ts
@@ -62,3 +62,86 @@ const world:WorldState =initWorld(5,5)
     console.log('Error', afterScriptWorld.error)
   }
 }
+
+{
+  console.log('')
+  console.log('=== EXAMPLE: ignore commands before place ====')
+  const commands = `MOVE
+          LEFT
+          MOVE
+          REPORT
+          PLACE 1,1,WEST
+          MOVE
+          RIGHT
+          REPORT`
+
+  console.log(loadCodeFromString(commands).join('\n'))
+
+  const afterScriptWorld = runScript(world, commands, (s) => {
+    console.log(s)
+  })
+
+  if (afterScriptWorld.error) {
+    console.log('Error', afterScriptWorld.error)
+  }
+}
+
+{
+  console.log('')
+  console.log('=== EXAMPLE: keep robot on board ====')
+  const commands = `
+          PLACE 0,0,SOUTH
+          MOVE
+          REPORT
+          MOVE
+          MOVE
+          REPORT
+          PLACE 4,4, NORTH
+          MOVE
+          MOVE
+          MOVE
+          REPORT
+          `
+
+  console.log(loadCodeFromString(commands).join('\n'))
+
+  const afterScriptWorld = runScript(world, commands, (s) => {
+    console.log(s)
+  })
+
+  if (afterScriptWorld.error) {
+    console.log('Error', afterScriptWorld.error)
+  }
+}
+
+{
+  console.log('')
+  console.log('=== EXAMPLE: error reporting ====')
+  const commands = `
+          PLACE 0,0,SOUTH
+          MOVE
+          REPORT
+          MOVE
+          MOVE
+          REPORT
+          PLACE 4,4, NORTH
+          MOVE
+          REPORT
+          ERROR1
+          ERROR2
+          MOVE
+          REPORT
+          `
+
+  console.log(loadCodeFromString(commands).join('\n'))
+
+  const afterScriptWorld = runScript(world, commands, (s) => {
+    console.log(s)
+  })
+
+  if (afterScriptWorld.error) {
+    console.log('Error', afterScriptWorld.error)
+    console.log('Output', afterScriptWorld.output)
+  }
+
+}
