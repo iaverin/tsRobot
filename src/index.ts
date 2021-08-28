e0cd8da4f4d9712e09ffefd873f4cad2046e7184
Ivan Averin <i.averin@gmail.com>
2021-08-28 12:04:32 +0300

    simple working app

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,21 @@
-import * as parser from './parser'
+import { DIRECTION, WorldState, runCommand } from "./botmaster"
 
-// main app
-
-/// types
-enum Direction {
-  NORTH = 'NORTH',
-  SOUTH = 'SOUTH',
-  EAST = 'EAST',
-  WEST = 'WEST'
-}
-
-type BotState = {
-  x: number
-  y: number
-  placed: boolean
-  facingDirection: Direction
-}
-
-type WorldState = {
-  board: {
-    width: number
-    height: number
-  }
-
-  bot: BotState
-}
-
-type Command = {
-  command: string
-  arguments: Array<number | Direction>
-}
-
-type commands = Array<Command>
-
-// init world
-
-let world: WorldState = {
-  board: {
-    height: 5,
+let world:WorldState ={
+  board:{
+    height:5,
     width: 5
   },
-
-  bot: {
-    x: 0,
-    y: 0,
-    facingDirection: Direction.NORTH,
-    placed: false
-  }
+  bot:{
+    x:0,
+    y:0,
+    placed:false,
+    facingDirection: DIRECTION.NORTH
+  },
+  error: null,
+  output: null 
 }
 
-const script = `
-    PLACE 0,0,NORTH
-    MOVE
-    REPORT
-    Output: 0,1,NORTH
-`
-
-const commandsInmplementation: parser.FunctionMapping = {
-  PLACE: {
-    description: 'Place the robot on board',
-    execute: (
-      world: WorldState,
-      x: number,
-      y: number,
-      direction: Direction
-    ) => {
-      if (
-        x >= 0 &&
-        x <= world.board.width &&
-        y >= 0 &&
-        y <= world.board.height
-      ) {
-        return {
-          ...world,
-          bot: {
-            x: x,
-            y: y,
-            facingDirection: direction,
-            placed: true
-          }
-        }
-      } else {
-        return {
-          ...world,
-          error: {
-            message: 'Placing coordinaties out of scope'
-          }
-        }
-      }
-    }
-  }
-}
-
-// while (nextCommand(world)){
-//     world = applyCommand(world, nextCommad)
-
-//     )
-
-// }
+world = runCommand(world, "PLACE 2 , 3, SOUTH")
+console.log("Error", world.error)
+console.log("Output", world.output)
+console.log("World", world)
